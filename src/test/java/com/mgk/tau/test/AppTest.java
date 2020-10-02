bd9bc882a57d5e247ce6ecc1ba710ecbd54803d1
Manoj Kolhe <manojgkolhe@gmail.com>
2020-10-02 21:41:32 +0530

    Updated code and added docker-grid.yml with VNC Viewer debuggable version

--- src/test/java/com/mgk/tau/test/AppTest.java
+++ src/test/java/com/mgk/tau/test/AppTest.java
@@ -7,14 +7,17 @@ import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Protocol;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Date;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 /**
  * Unit test for simple App.
@@ -27,11 +30,12 @@ public class AppTest {
     ExtentReports extentReports;
     ExtentTest test;
 
+
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue() throws MalformedURLException, InterruptedException {
+    public void runTestWithDocker() throws MalformedURLException, InterruptedException {
 
         extentReporter = new ExtentSparkReporter(System.getProperty("user.dir") + "/testReport.html");
         extentReports = new ExtentReports();
@@ -64,6 +68,55 @@ public class AppTest {
         URL remoteAddress;
         webDriver = new RemoteWebDriver(new URL(nodeURL), chromeCapabilities);
        // webDriver = new RemoteWebDriver(new URL(nodeURL), firefoxCapabilities);
+
+        webDriver.get(baseURL);
+        WebElement webElement = webDriver.findElement(By.name("q"));
+
+        webElement.sendKeys("Manoj Kolhe");
+        webElement.sendKeys(Keys.ENTER);
+        //Thread.sleep(10000L);
+        String pageSource = webDriver.getPageSource();
+        Assert.assertTrue(pageSource.contains("Manoj Kolhe"));
+        webDriver.close();
+        webDriver.quit();
+
+
+        extentReports.flush();
+
+    }
+
+
+    @Test
+    public void runTestLocally() throws MalformedURLException, InterruptedException {
+
+        extentReporter = new ExtentSparkReporter(System.getProperty("user.dir") + "/testReport.html");
+        extentReports = new ExtentReports();
+        extentReports.attachReporter(extentReporter);
+
+        extentReports.setSystemInfo("OS", "Windows");
+        extentReports.setSystemInfo("browser", "Chrome");
+
+        extentReporter.config().setOfflineMode(true);
+        extentReporter.config().setDocumentTitle("Extent Report Demo");
+        extentReporter.config().setReportName("Test Report");
+        extentReporter.config().setProtocol(Protocol.HTTPS);
+
+        extentReporter.config().setTheme(Theme.DARK);
+        extentReporter.config().setTimeStampFormat("EEEE, MMMM dd, yyyy, hh:mm a '('zzz')'");
+
+
+        test = extentReports.createTest(new Object() {
+        }.getClass().getEnclosingMethod().getName(), "Executed on " + new Date().toLocaleString());
+
+        baseURL = "https://www.google.com";
+
+
+
+
+        WebDriverManager.chromedriver().setup();
+
+        webDriver = new ChromeDriver();
+
 
         webDriver.get(baseURL);
         WebElement webElement = webDriver.findElement(By.name("q"));
